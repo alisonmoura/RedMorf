667e1a01bf979502e686a4fd537b1b0466e3eb4c
Alison Moura <alison.oghino@gmail.com>
2017-03-25 18:47:59 -0300

    Primeiro commit - inicializando projeto no Spring Boot com a entidade Usuario

---
+++ src/main/java/com/redmorf/RedMorfApplication.java
@@ -0,0 +1,12 @@
+package com.redmorf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedMorfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedMorfApplication.class, args);
+	}
+}
