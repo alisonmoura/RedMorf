f5d0597a8bf70d2d3915e4c3fecf95e1d6c29b3d
Alison Moura <alison.oghino@gmail.com>
2017-04-01 18:56:13 -0300

    Criando estrutura da JPA na classe Venda

--- src/main/java/com/redmorf/model/Venda.java
+++ src/main/java/com/redmorf/model/Venda.java
@@ -1,5 +1,71 @@
 package com.redmorf.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "venda")
 public class Venda {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_venda")
+	@SequenceGenerator(name = "seq_venda", sequenceName = "seq_venda", initialValue = 1, allocationSize = 50)
+	private Long id;
+	@Column(nullable=false)
+	private Date dataVenda;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDataVenda() {
+		return dataVenda;
+	}
+
+	public void setDataVenda(Date dataVenda) {
+		this.dataVenda = dataVenda;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataVenda == null) ? 0 : dataVenda.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Venda other = (Venda) obj;
+		if (dataVenda == null) {
+			if (other.dataVenda != null)
+				return false;
+		} else if (!dataVenda.equals(other.dataVenda))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
